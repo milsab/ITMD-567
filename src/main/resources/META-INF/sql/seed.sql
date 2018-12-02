3e04c317a434c02d92332c36214018e72b0bac60
msabouri@hawk.iit.edu <milad.sabouri@gmail.com>
2018-12-02 00:20:03 -0600

    Change table name to uppercase letters

--- src/main/resources/META-INF/sql/seed.sql
+++ src/main/resources/META-INF/sql/seed.sql
@@ -17,23 +17,23 @@ insert ignore into sec_user_groups(username, groupname) values('seller2','SELLER
 insert ignore into sec_user_groups(username, groupname) values('milad','SELLER');
 insert ignore into sec_user_groups(username, groupname) values('admin','ADMINS');
 
-insert into buyer(buyerid, birthday, firstname, lastname, gender, username) values(1, '1970-09-01', 'Alex', 'Wooden', 'male', 'buyer1');
-insert into buyer(buyerid, birthday, firstname, lastname, gender, username) values(2, '1980-09-01', 'Jack', 'Smith', 'male', 'buyer2');
-insert into seller(sellerid, birthday, firstname, lastname, gender, username) values(1, '1970-09-01', 'Chris', 'Hamilton', 'male', 'seller1');
-insert into seller(sellerid, birthday, firstname, lastname, gender, username) values(2, '1980-09-01', 'Richard', 'Rex', 'male', 'seller2');
+insert into BUYER(buyerid, birthday, firstname, lastname, gender, username) values(1, '1970-09-01', 'Alex', 'Wooden', 'male', 'buyer1');
+insert into BUYER(buyerid, birthday, firstname, lastname, gender, username) values(2, '1980-09-01', 'Jack', 'Smith', 'male', 'buyer2');
+insert into SELLER(sellerid, birthday, firstname, lastname, gender, username) values(1, '1970-09-01', 'Chris', 'Hamilton', 'male', 'seller1');
+insert into SELLER(sellerid, birthday, firstname, lastname, gender, username) values(2, '1980-09-01', 'Richard', 'Rex', 'male', 'seller2');
 
-insert into food(id, date_cooked, description, name, seller_id) values(1, '2018-11-01', 'includes meat', 'Meat Ball', 1);
-insert into food(id, date_cooked, description, name, seller_id) values(2, '2018-11-02', 'includes Vegi', 'Vegi Burger', 1);
+insert into FOOD(id, date_cooked, description, name, seller_id) values(1, '2018-11-01', 'includes meat', 'Meat Ball', 1);
+insert into FOOD(id, date_cooked, description, name, seller_id) values(2, '2018-11-02', 'includes Vegi', 'Vegi Burger', 1);
 
-insert into offer(offer_id, createddate, description, quantity, title, unitprice, seller_id, recipe, image_file) values(1, '2018-11-01', 'includes meat', 1, 'Meat Offer',  10, 1, "RECIPE 1", "steak.jpg");
-insert into offer(offer_id, createddate, description, quantity, title, unitprice, seller_id, recipe, image_file) values(2, '2018-11-02', 'includes vegi', 1, 'Vegi Offer',  5, 1, "RECIPE 2", "salad.jpeg");
-insert into offer(offer_id, createddate, description, quantity, title, unitprice, seller_id, recipe, image_file) values(3, '2018-11-03', 'includes egg', 1, 'Egg Offer',  3, 1, "RECIPE 3", "tahchin.jpg");
+insert into OFFER(offer_id, createddate, description, quantity, title, unitprice, seller_id, recipe, image_file) values(1, '2018-11-01', 'includes meat', 10, 'Meat Offer',  10, 1, "RECIPE 1", "steak.jpg");
+insert into OFFER(offer_id, createddate, description, quantity, title, unitprice, seller_id, recipe, image_file) values(2, '2018-11-02', 'includes vegi', 14, 'Vegi Offer',  5, 1, "RECIPE 2", "salad.jpeg");
+insert into OFFER(offer_id, createddate, description, quantity, title, unitprice, seller_id, recipe, image_file) values(3, '2018-11-03', 'includes egg', 8, 'Egg Offer',  3, 1, "RECIPE 3", "tahchin.jpg");
 
 insert into food_offer(offer_id, food_id) values(1, 1);
 insert into food_offer(offer_id, food_id) values(2, 2);
 
-insert into order_food(order_id, order_date, price, quantity, buyer_id, offer_id) values(1, '2018-11-01', 50, 2, 1,  1);
-insert into order_food(order_id, order_date, price, quantity, buyer_id, offer_id) values(2, '2018-11-02', 75, 6, 1,  2);
-insert into order_food(order_id, order_date, price, quantity, buyer_id, offer_id) values(3, '2018-11-03', 80, 3, 1,  3);
+insert into ORDER_FOOD(order_id, order_date, price, quantity, buyer_id, offer_id) values(1, '2018-11-01', 20, 2, 1,  1);
+insert into ORDER_FOOD(order_id, order_date, price, quantity, buyer_id, offer_id) values(2, '2018-11-02', 30, 6, 1,  2);
+insert into ORDER_FOOD(order_id, order_date, price, quantity, buyer_id, offer_id) values(3, '2018-11-03', 9, 3, 1,  3);
 
 insert into recipe(food_id, recipes) values(1, 'beef');
